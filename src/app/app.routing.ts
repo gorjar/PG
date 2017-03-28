cd939583799e64122134432f100ec9daafc8531d
Adam Matyka <adam.matyka@gmail.com>
2017-03-28 21:16:18 +0200

    zaprojektowanie menu aplikacji

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,10 @@ import {Routes, RouterModule} from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { StudentsComponent } from './students/students.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const appRoutes: Routes = [
   {
@@ -12,6 +16,22 @@ const appRoutes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'students',
+    component: StudentsComponent
+  },
+  {
+    path: 'subjects',
+    component: SubjectsComponent
+  },
+  {
+    path: 'schedule',
+    component: ScheduleComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
   }
 ];
 

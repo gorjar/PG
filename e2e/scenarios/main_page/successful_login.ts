9747f32cf3c060d4689c7abda6efc3fe329d513f
Adam Matyka <adam.matyka@gmail.com>
2017-04-21 00:48:09 +0200

    testy - logowanie, wylogowanie i sprawdzenie komponentow

--- e2e/scenarios/main_page/successful_login.ts
+++ e2e/scenarios/main_page/successful_login.ts
@@ -18,6 +18,8 @@ describe('Dziennik Lekcyjny Main Page', () => {
     let SETTINGS_BUTTON = element(by.id('settings'));
     let ABOUT_BUTTON = element(by.id('about'));
 
+    let until = protractor.ExpectedConditions;
+
     beforeEach(() => {
         browser.get(browser.baseUrl);
     });
@@ -33,14 +35,90 @@ describe('Dziennik Lekcyjny Main Page', () => {
         PASSWORD_FIELD.sendKeys(LoginData.correct_password);
         expect(SIGN_IN_BUTTON.isPresent()).toBe(true);
         SIGN_IN_BUTTON.click();
-
-        //expect(STUDENTS_BUTTON.isPresent()).toBe(true);
-
+        //browser.wait(until.presenceOf(STUDENTS_BUTTON), 5000, 'Taking too long to load element');
     });
-    /*it('logout', () => {
-        expect(LOGOUT_BUTTON.isPresent()).toBe(true);
-        LOGOUT_BUTTON.click();
-    });*/
+
+
+  it('logout', () => {
+    expect(LOGIN_BUTTON.isPresent()).toBe(true);
+    LOGIN_BUTTON.click();
+    expect(EMAIL_FIELD.isPresent()).toBe(true, "Display email field");
+    EMAIL_FIELD.sendKeys(LoginData.correct_login);
+    PASSWORD_FIELD.sendKeys(LoginData.correct_password);
+    expect(SIGN_IN_BUTTON.isPresent()).toBe(true);
+    SIGN_IN_BUTTON.click();
+    browser.wait(until.presenceOf(LOGOUT_BUTTON), 5000, 'Taking too long to load element');
+    expect(LOGOUT_BUTTON.isPresent()).toBe(true);
+    LOGOUT_BUTTON.click();
+  });
+
+  it('student component', () => {
+    expect(LOGIN_BUTTON.isPresent()).toBe(true);
+    LOGIN_BUTTON.click();
+    expect(EMAIL_FIELD.isPresent()).toBe(true, "Display email field");
+    EMAIL_FIELD.sendKeys(LoginData.correct_login);
+    PASSWORD_FIELD.sendKeys(LoginData.correct_password);
+    expect(SIGN_IN_BUTTON.isPresent()).toBe(true);
+    SIGN_IN_BUTTON.click();
+    browser.wait(until.presenceOf(STUDENTS_BUTTON), 5000, 'Taking too long to load element');
+    expect(STUDENTS_BUTTON.isPresent()).toBe(true);
+  });
+
+  it('subjects component', () => {
+    expect(LOGIN_BUTTON.isPresent()).toBe(true);
+    LOGIN_BUTTON.click();
+    expect(EMAIL_FIELD.isPresent()).toBe(true, "Display email field");
+    EMAIL_FIELD.sendKeys(LoginData.correct_login);
+    PASSWORD_FIELD.sendKeys(LoginData.correct_password);
+    expect(SIGN_IN_BUTTON.isPresent()).toBe(true);
+    SIGN_IN_BUTTON.click();
+    browser.wait(until.presenceOf(SUBJECTS_BUTTON), 5000, 'Taking too long to load element');
+    expect(SUBJECTS_BUTTON.isPresent()).toBe(true);
+  });
+
+  it('schedule component', () => {
+    expect(LOGIN_BUTTON.isPresent()).toBe(true);
+    LOGIN_BUTTON.click();
+    expect(EMAIL_FIELD.isPresent()).toBe(true, "Display email field");
+    EMAIL_FIELD.sendKeys(LoginData.correct_login);
+    PASSWORD_FIELD.sendKeys(LoginData.correct_password);
+    expect(SIGN_IN_BUTTON.isPresent()).toBe(true);
+    SIGN_IN_BUTTON.click();
+    browser.wait(until.presenceOf(SCHEDULE_BUTTON), 5000, 'Taking too long to load element');
+    expect(SCHEDULE_BUTTON.isPresent()).toBe(true);
+  });
+
+  it('settings component', () => {
+    expect(LOGIN_BUTTON.isPresent()).toBe(true);
+    LOGIN_BUTTON.click();
+    expect(EMAIL_FIELD.isPresent()).toBe(true, "Display email field");
+    EMAIL_FIELD.sendKeys(LoginData.correct_login);
+    PASSWORD_FIELD.sendKeys(LoginData.correct_password);
+    expect(SIGN_IN_BUTTON.isPresent()).toBe(true);
+    SIGN_IN_BUTTON.click();
+    browser.wait(until.presenceOf(SETTINGS_BUTTON), 5000, 'Taking too long to load element');
+    expect(SETTINGS_BUTTON.isPresent()).toBe(true);
+  });
+
+  it('about component', () => {
+    expect(LOGIN_BUTTON.isPresent()).toBe(true);
+    LOGIN_BUTTON.click();
+    expect(EMAIL_FIELD.isPresent()).toBe(true, "Display email field");
+    EMAIL_FIELD.sendKeys(LoginData.correct_login);
+    PASSWORD_FIELD.sendKeys(LoginData.correct_password);
+    expect(SIGN_IN_BUTTON.isPresent()).toBe(true);
+    SIGN_IN_BUTTON.click();
+    browser.wait(until.presenceOf(ABOUT_BUTTON), 5000, 'Taking too long to load element');
+    expect(ABOUT_BUTTON.isPresent()).toBe(true);
+  });
+
+
+
+
+
+
+
+
 
 
 

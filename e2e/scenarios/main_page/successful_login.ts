4cb2f0acc0be1a164e6c9a30ba88c03c00f92271
Adam Matyka <adam.matyka@gmail.com>
2017-04-20 21:26:57 +0200

    testy - logowanie

--- e2e/scenarios/main_page/successful_login.ts
+++ e2e/scenarios/main_page/successful_login.ts
@@ -7,9 +7,11 @@ import {LoginData} from "../../feed/test_data"
 
 describe('Dziennik Lekcyjny Main Page', () => {
 
-    let LOGIN_BUTTON = element(by.id('login_button'));
-    let EMAIL_FIELD = element(by.id('Email'));
-    let PASSWORD_FIELD = element(by.id('Passwd'));
+    let LOGIN_BUTTON = element(by.id('signin'));
+    let LOGOUT_BUTTON = element(by.id('logout'));
+    let EMAIL_FIELD = element(by.id('email'));
+    let SIGN_IN_BUTTON = element(by.id('sign-in'));
+    let PASSWORD_FIELD = element(by.id('password'));
     let STUDENTS_BUTTON = element(by.id('students'));
     let SUBJECTS_BUTTON = element(by.id('subjects'));
     let SCHEDULE_BUTTON = element(by.id('schedule'));
@@ -20,29 +22,26 @@ describe('Dziennik Lekcyjny Main Page', () => {
         browser.get(browser.baseUrl);
     });
 
-    it('Log-in to application and verify that all elements are displayed ', () => {
+
+
+
+    it('login', () => {
         expect(LOGIN_BUTTON.isPresent()).toBe(true);
         LOGIN_BUTTON.click();
+        expect(EMAIL_FIELD.isPresent()).toBe(true, "Display email field");
+        EMAIL_FIELD.sendKeys(LoginData.correct_login);
+        PASSWORD_FIELD.sendKeys(LoginData.correct_password);
+        expect(SIGN_IN_BUTTON.isPresent()).toBe(true);
+        SIGN_IN_BUTTON.click();
 
-        browser.getAllWindowHandles().then(function(handles) {
-            browser.switchTo().window(handles[1]).then(function () {
-                expect(EMAIL_FIELD.isPresent()).toBe(true, "Display email field");
-                EMAIL_FIELD.sendKeys(LoginData.correct_login);
-                EMAIL_FIELD.submit();
-                browser.wait(protractor.ExpectedConditions.visibilityOf(PASSWORD_FIELD));
+        //expect(STUDENTS_BUTTON.isPresent()).toBe(true);
 
-                expect(PASSWORD_FIELD.isPresent()).toBe(true, "Display password field");
-                PASSWORD_FIELD.sendKeys(LoginData.correct_password);
-                PASSWORD_FIELD.submit();
-                expect(PASSWORD_FIELD.isPresent()).toBe(false, "Display password field after log-in");
-            });
-            browser.switchTo().window(handles[0]).then(function () {
-                browser.wait(protractor.ExpectedConditions.visibilityOf(STUDENTS_BUTTON));
-                expect(SUBJECTS_BUTTON.isPresent()).toBe(true, "Display email field");
-                expect(SCHEDULE_BUTTON.isPresent()).toBe(true, "Display email field");
-                expect(SETTINGS_BUTTON.isPresent()).toBe(true, "Display email field");
-                expect(ABOUT_BUTTON.isPresent()).toBe(true, "Display email field");
-            });
-        });
     });
+    /*it('logout', () => {
+        expect(LOGOUT_BUTTON.isPresent()).toBe(true);
+        LOGOUT_BUTTON.click();
+    });*/
+
+
+
 });

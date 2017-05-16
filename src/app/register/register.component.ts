f07b55dbc314a7420cbb97908b256f1e377331c6
Adam Matyka <adam.matyka@gmail.com>
2017-05-16 22:31:03 +0200

    e-mail registration via Angularfire2

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import {User} from '../user';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  email: string;
+  password: string;
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(email: string, password: string){
+    this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+    console.log('ok');
+  }
+
+}

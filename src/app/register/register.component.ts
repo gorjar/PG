3cd5a9bd0a6290989db4cd2a7c7643cd279809b2
Adam Matyka <adam.matyka@gmail.com>
2017-06-15 22:42:52 +0200

    register form customization

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
 import { ServerService } from '../server.service';
-import * as firebase from 'firebase';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,24 +11,47 @@ import * as firebase from 'firebase';
 })
 export class RegisterComponent implements OnInit {
 
+  myForm: FormGroup;
   users:any;
-
-  name:string='';
-  lastname:string='';
-  email:string='';
-  password:string='';
-  confirmpassword:string='';
   role:string='guest';
-
 
   constructor(
     private afAuth: AngularFireAuth,
     private serverService: ServerService,
-    private router:Router
+    private router:Router,
+    private fb: FormBuilder
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): any {
+    this.myForm = this.fb.group({
+      name: [''],
+      lastname: [''],
+      email: ['', Validators.compose([
+        Validators.required,
+        this.isEmail
+      ])],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.compose([
+        Validators.required,
+        this.isEqualPassword.bind(this)
+      ])],
+    });
+  }
 
+  isEmail(control: FormControl): { [s: string]: boolean } {
+    if (!control.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+      return {noEmail: true};
+    }
+  }
+
+  isEqualPassword(control: FormControl): { [s: string]: boolean } {
+    if (!this.myForm) {
+      return {passwordsNotMatch: true};
+
+    }
+    if (control.value !== this.myForm.controls['password'].value) {
+      return {passwordsNotMatch: true};
+    }
   }
 
   onRegisterSubmit( email, password){
@@ -39,9 +62,9 @@ export class RegisterComponent implements OnInit {
     });
     this.serverService.addUser(
       {
-        name: this.name,
-        lastname: this.lastname,
-        email: this.email,
+        name: this.myForm.value.name,
+        lastname: this.myForm.value.lastname,
+        email: this.myForm.value.email,
         role: this.role
       }
     );

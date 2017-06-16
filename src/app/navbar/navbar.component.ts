bd3422c202173231738323cdf521b7bb9043f99f
Adam Matyka <adam.matyka@gmail.com>
2017-06-16 23:14:58 +0200

    small text change

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -25,7 +25,7 @@ export class NavbarComponent implements OnInit {
     this.serverService.currentUserMail='';
     this.serverService.currentUserRole='';
     this.afAuth.auth.signOut();
-    this.flashMessage.show('Nastąpiło udane wylogowanie',
+    this.flashMessage.show('Nastąpiło poprawne wylogowanie',
       {cssClass: 'alert-success', timeout: 3000});
   }
 

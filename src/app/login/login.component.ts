2ceca317706fd74dc96d5849f11b8a06fd8d0000
Adam Matyka <adam.matyka@gmail.com>
2017-06-21 21:36:51 +0200

    #89 login form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,8 +11,17 @@ import {FlashMessagesService} from 'angular2-flash-messages';
 })
 export class LoginComponent implements OnInit {
 
-  email: string;
+  email:string='';
+  emailMsg:string='';
+  emailB:boolean;
+  emailC:number=0;
+
   password: string;
+  passwordMsg:string='';
+  passwordB:boolean;
+  passwordC:number=0;
+
+  allValidator:boolean;
 
   constructor(
     private afAuth: AngularFireAuth,
@@ -22,7 +31,54 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.emailValidator();
+    this.passwordValidator();
 
+  }
+
+  allValidatorF(){
+    if ( this.emailB==true || this.passwordB==true){
+      this.allValidator=true
+    } else {
+      this.allValidator=false;
+    }
+  }
+
+  emailValidator(){
+    if (!this.email.match(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+      this.emailMsg='Email jest nieprawidłowy';
+      this.emailB=true;
+      this.emailC = this.emailC+1;
+      this.allValidatorF();
+    }
+    else {
+      this.emailMsg='';
+      this.emailB=false;
+      this.allValidatorF();
+    }
+  }
+
+  passwordValidator(){
+    if (this.password.length<6){
+      this.passwordMsg='Hasło powinno zawierać conajmniej 6 znaków';
+      this.passwordB=true;
+      this.passwordC = this.passwordC+1;
+      this.allValidatorF();
+    }
+    else {
+      if (!this.password.match(/^[a-z0-9]+$/i)){
+        this.passwordMsg='Hasło powinno składać się jedynie ze znaków alfanumerycznych';
+        this.passwordB=true;
+        this.passwordC = this.passwordC+1;
+        this.allValidatorF();
+      }
+      else {
+        this.passwordMsg='';
+        this.passwordB=false;
+        this.allValidatorF();
+      }
+    }
   }
 
   onLoginSubmit(){
